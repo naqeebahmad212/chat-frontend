4a4f9e6cec40b006898035373bac5ea7ce03c2fa
Naqeeb <naqeebahmad212@gmail.com>
2024-06-15 21:16:47 +0500

    ui chnages

--- components/Mian.tsx
+++ components/Mian.tsx
@@ -79,15 +79,18 @@ const Mian = ({
     socket.on("connect", () => {
       console.log(socket.id, "connected");
     });
+
     socket.on("messageReceived", (data) => {
       setLastMessage(data);
-      // if (inputRef.current) inputRef.current.focus();
+      setMessage({ text: "", senderId: "", reciverId: "" });
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+
       if (
         data.senderId === currentUser.id ||
         (data.senderId === activeChat?.id && data.reciverId === currentUser.id)
       ) {
-        setMessage((message) => ({ ...message, text: "" }));
-
         if (data.senderId !== currentUser.id) {
           setMessages((messages) => [...messages, data]);
         }
@@ -187,7 +190,7 @@ const Mian = ({
         </div>
       </div>
 
-      <div className="message-container custom-scrollbar h-[78%] overflow-y-auto bg-gray-0">
+      <div className="message-container custom-scrollbar h-[80%] overflow-y-auto bg-gray-0">
         <div className="p-2">
           {messages.length > 0 &&
             messages
@@ -249,19 +252,18 @@ const Mian = ({
       >
         <div className="input w-full  h-[10%] flex items-center justify-around gap-1 ">
           <input
-            ref={inputRef}
             type="text"
             className="w-full rounded-full outline-none p-2 px-4 border "
             placeholder="Type your message here"
             onChange={(e) =>
               setMessage({
-                ...message,
                 text: e.target.value,
                 senderId: currentUser.id,
                 reciverId: activeChat.id,
               })
             }
             value={message.text}
+            ref={inputRef}
           />
           <button
             type="submit"
